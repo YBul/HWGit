83108ce139ebe99a4ca420a64d988b20cc5ad245
YBul <bulatova0306@gmail.com>
2022-01-31 15:04:20 +0200

    Прошла лекция 5

---
+++ src/lesson5/Robot.java
@@ -0,0 +1,54 @@
+//package lesson5;
+//
+//public class Robot {
+//    String name;
+//    float speed;
+//    String color;
+//    int countHands;
+//    int height;
+//    Weapon laser = new Weapon();
+//
+//    public Robot() {
+//        System.out.println("I'm constructor");
+//        speak();
+//    }
+//
+//    public Robot(String data) {
+//        System.out.println("Your data is " + data);
+//    }
+//
+//    public Robot(String name, float speed, String color, int countHands, int height) {
+//        this.name = name;
+//        this.speed = speed;
+//        this.color = color;
+//        this.countHands = countHands;
+//        this.height = height;
+//    }
+//
+//    void speak() {
+//        System.out.println(name + " says something");
+//    }
+//
+//    void run() {
+//        System.out.println(name + " run with speed " + speed);
+//    }
+//
+//    void repair() {
+//            System.out.println(name + " repair something with self " + countHands + " hand(-s");
+//        }
+//
+//        int sumDigits(int a, int b) {
+//        return a + b;
+//        }
+//
+//        public class Weapon{
+//        int weight;
+//        int damage;
+//        String name;
+//
+//        void fire() {
+//            System.out.println(name + " get damage is " + damage);
+//        }
+//        }
+//    }
+//
---
+++ src/lesson5/WorkerClass.java
@@ -0,0 +1,46 @@
+//package lesson5;
+//
+//import java.util.Arrays;
+//
+//public class WorkerClass {
+//
+//    public static void main(String[] args) {
+//        Robot robot1 = new Robot();
+//        Robot robot2 = new Robot();
+//
+//        Robot robot3 = new Robot("Robot", 19.99f, "red", 10, 541);
+//        robot3.speak();
+//        robot3.run();
+//
+////        robot1.name = "Boris";
+////        robot1.speed = 15.89f;
+////        robot1.color = "White";
+////        robot1.countHands = 3;
+////        robot1.height = 189;
+////
+////        robot2.name = "Alex";
+////        robot2.speed = 29.5f;
+////        robot2.color = "Black";
+////        robot2.countHands = 5;
+////        robot2.height = 210;
+//
+////        System.out.println("What is your name? > " + robot1.name);
+////        System.out.println("What is your name? > " + robot1.color);
+////        System.out.println("****************");
+////        System.out.println("What is your name? > " + robot2.name);
+////        System.out.println("What is your name? > " + robot2.color);
+//
+////        robot1.speak();
+////        robot2.run();
+////        robot1.repair();
+//
+//        robot1.laser.name = "DamageDiller";
+//        robot1.laser.damage = 150;
+//
+//        System.out.println(robot1.laser.name + " have damage is " + robot1.laser.damage);
+//        robot1.laser.fire();
+//
+//        System.out.println("Result from r1: " + robot1.sumDigits(15,5));
+//        System.out.println("Result from r2: " + robot1.sumDigits(25,45));
+//    }
+//}
